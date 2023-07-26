99bb112559e7bde641f0798290c98ddf76cb0a30
Darío Beiró <dario.beiro@dxc.com>
2023-07-26 12:22:37 +0200

    green on case under six

--- src/core/password.ts
+++ src/core/password.ts
@@ -3,8 +3,12 @@ export class Password {
     constructor() {}
 
     public isValid(password: string): boolean {
-        if(!!!password) return false
+        if(!!!password || this.isUnderSix(password)) return false
 
         return true;
+    }
+
+    private isUnderSix(password: string): boolean {
+        return password.length <= 6
     }
 }
